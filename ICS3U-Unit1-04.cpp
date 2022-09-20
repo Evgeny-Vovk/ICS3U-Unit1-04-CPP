156ff9f04ea9d9b98d6b9962805a66c7c22c5bf2
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-09-20 15:26:09 +0000

    done with cpp unit1-04

---
+++ ICS3U-Unit1-04.cpp
@@ -0,0 +1,15 @@
+// Copyright(c) 2022 Evgeny Vovk All rights reserved
+//
+// Created by : Evgeny Vovk
+// Created on : September 2022
+// ICS3U-Unit1-03.cpp File, learning how style properly in C++
+
+#include <iostream>
+
+int main() {
+    std::cout << "The width of a square is 5 cm and the length is 9 cm.\n";
+    std::cout << "The area of the square is " << (5 * 9) << "cm².\n";
+    std::cout << "The perimeter of the square is " << (2 * (9 + 5));
+
+    std::cout << "\nDone.\n";
+}
