1f9cac0971a52d7c297f4e7c1baab94122a3f3fa
Evgeny Vovk <Evgeny.vovk@mths.ca>
2022-09-22 15:26:13 +0000

    fix

--- ICS3U-Unit1-04.cpp
+++ ICS3U-Unit1-04.cpp
@@ -7,9 +7,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "The width of a square is 5 cm and the length is 9 cm.\n";
-    std::cout << "The area of the square is " << (5 * 9) << "cm².\n";
-    std::cout << "The perimeter of the square is " << (2 * (9 + 5));
+    std::cout << "The width of a square is 5 cm and the length is 3 cm.\n";
+    std::cout << "The area of the square is " << (5 * 3) << "cm².\n";
+    std::cout << "The perimeter of the square is " << (2 * (3 + 5)) << "cm.";
 
     std::cout << "\nDone.\n";
 }
